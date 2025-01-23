8eac4cf7e30aec6c496dabeddf9748610cbd49c0
tsujigiri15 <160392248+tsujigiri15@users.noreply.github.com>
2025-01-23 23:23:07 +0000

    Update stuff_mover.sh

--- stuff_mover.sh
+++ stuff_mover.sh
@@ -4,16 +4,16 @@ mkdir ~/.icons
 mkdir ~/.themes
 mkdir ~/Pictures
 
-mv -r Themes_other/Nordzy-dark ~/.icons
-mv -r Themes_other/ComixCursors-White ~/.icons
-mv -r Themes_other/candy-icons ~/.icons
-mv -r Themes_other/BeautySolar ~/.icons
+mv Themes_other/Nordzy-dark ~/.icons
+mv Themes_other/ComixCursors-White ~/.icons
+mv Themes_other/candy-icons ~/.icons
+mv Themes_other/BeautySolar ~/.icons
 
-mv -r Themes_other/Everforest-Dark-Medium ~/.themes
-mv -r Themes_other/Gruvbox-Dark-Medium ~/.themes
-mv -r Themes_other/Nordic-bluish-v40 ~/.themes
-mv -r Themes_other/Solarized-DarkArc-Orange ~/.themes
-mv -r Themes_other/Gruvbox-Dark-Medium ~/.themes
+mv Themes_other/Everforest-Dark-Medium ~/.themes
+mv Themes_other/Gruvbox-Dark-Medium ~/.themes
+mv Themes_other/Nordic-bluish-v40 ~/.themes
+mv Themes_other/Solarized-DarkArc-Orange ~/.themes
+mv Themes_other/Gruvbox-Dark-Medium ~/.themes
 
 mv Themes_other/longestwp.jpg ~/Pictures
 mv Themes_other/numen_stone.jpg ~/Pictures
