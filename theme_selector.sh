059a1cb5c55a16b5297cc41dfa5746ef27b03bd2
tsujigiri15 <160392248+tsujigiri15@users.noreply.github.com>
2025-01-26 16:03:08 +0000

    Added flatpak themes

--- theme_selector.sh
+++ theme_selector.sh
@@ -12,6 +12,8 @@ then
 	cp rofi/config.rasi.cyberneon rofi/config.rasi
 	cp waybar/style.css.cyberneon waybar/style.css
 	sed -i '7s/appear.*/appear_cyberneon/' $HOME/.config/sway/config
+ 	sudo flatpak override --env=GTK_THEME=materia-cyberpunk-neon
+        sudo flatpak override --env=ICON_THEME=candy-icons
 
 echo "Engaging Cyberpunk!"
 
@@ -24,6 +26,8 @@ then
 	cp rofi/config.rasi.everf rofi/config.rasi
 	cp waybar/style.css.everf waybar/style.css
 	sed -i '7s/appear.*/appear_everforest/' $HOME/.config/sway/config
+        sudo flatpak override --env=GTK_THEME=Everforest-Dark-Medium
+        sudo flatpak override --env=ICON_THEME=Gruvbox-Plus-Dark
 
 echo "Going to the forest"
 
@@ -36,6 +40,8 @@ then
 	cp rofi/config.rasi.gruv rofi/config.rasi
 	cp waybar/style.css.gruv waybar/style.css
 	sed -i '7s/appear.*/appear_gruvbox/' $HOME/.config/sway/config
+	sudo flatpak override --env=GTK_THEME=Gruvbox-Dark-Medium
+        sudo flatpak override --env=ICON_THEME=Gruvbox-Plus-Dark
 
 echo "Opening the gruvbox"
 
@@ -48,6 +54,8 @@ then
 	cp rofi/config.rasi.nord rofi/config.rasi
 	cp waybar/style.css.nord waybar/style.css
 	sed -i '7s/appear.*/appear_nord/' $HOME/.config/sway/config
+	sudo flatpak override --env=GTK_THEME=Nordic-bluish-v40
+        sudo flatpak override --env=ICON_THEME=Nordzy-dark
 
 echo "Nord"
 
@@ -60,6 +68,8 @@ then
 	cp rofi/config.rasi.nord rofi/config.rasi
 	cp waybar/style.css.nordmod waybar/style.css
 	sed -i '7s/appear.*/appear_nord/' $HOME/.config/sway/config
+	sudo flatpak override --env=GTK_THEME=Nordic-bluish-v40
+        sudo flatpak override --env=ICON_THEME=Nordzy-dark
 
 echo "Modifying Nord with cooler terminal and bar"
 
@@ -73,6 +83,8 @@ then
 	cp rofi/config.rasi.solarized rofi/config.rasi
 	cp waybar/style.css.solarized waybar/style.css
 	sed -i '7s/appear.*/appear_solarized/' $HOME/.config/sway/config
+	sudo flatpak override --env=GTK_THEME=Solarized-DarkArc-Orange
+        sudo flatpak override --env=ICON_THEME=BeautySolar
 
 echo "Becoming solarized"
 
