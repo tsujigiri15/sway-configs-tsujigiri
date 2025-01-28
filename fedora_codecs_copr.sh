be3ce7e2247f94527c6572ef2e50facd633d1bbd
tsujigiri15 <160392248+tsujigiri15@users.noreply.github.com>
2025-01-28 23:34:41 +0000

    Update fedora_codecs_copr.sh

--- fedora_codecs_copr.sh
+++ fedora_codecs_copr.sh
@@ -2,6 +2,7 @@
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+sudo dnf update @core
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
 sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
